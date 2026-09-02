e3c6312836da037868b50b1af75a54d3862b06f9
Subhadip Biswas <bsubhadip114@gmail.com>
2026-09-02 23:12:53 +0530

    Implement number guessing game in NumberGame.java

---
+++ CODSOFT_Number_Game/NumberGame.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+
+public static void main(String[] args) {
+
+	NumberGame game = new NumberGame();
+
+	Scanner sc = new Scanner(System.in);
+	while(true){
+		System.out.println("\n||---------------------------||");
+		System.out.println("|| CAN YOU GUESS THE NUMBER? ||");
+		System.out.println("||---------------------------||\n");
+		System.out.println("1. Start the game");
+		System.out.println("2. Exit the game");
+		System.out.print("\nEnter your choice (1/2): ");
+		int choice = sc.nextInt();
+		sc.nextLine();
+
+		switch(choice){
+			case 1:
+				game.startGame();
+				break;
+
+			case 2:
+				System.out.println("\nGame Exited\n");
+				return;
+
+			default:
+				System.out.println("\nEnter Valid choice\n");
+				break;
+		}
+	}
+}
+
+public void startGame(){
+	Scanner sc = new Scanner(System.in);
+	Random random = new Random();
+	int randomnumber = random.nextInt(1,100);
+	System.out.println("\n||--------------------------------||");
+	System.out.println("|| GAME STARTED. GUESS THE NUMBER ||");
+	System.out.println("||--------------------------------||\n");
+	System.out.print("\nGuess the number: ");
+		int usernumber = sc.nextInt();
+		sc.nextLine();
+		while (randomnumber != usernumber){
+			int temp = usernumber;
+			if(temp>randomnumber){
+				System.out.println("Your number is too high");
+			}
+			else{
+				System.out.println("Your number is too low");
+			}
+			System.out.print("\nGuess again: ");
+			usernumber = sc.nextInt();
+			sc.nextLine();
+			}
+		System.out.println("\n\nCongratulations. Your guess is right the number is: "+ randomnumber+"\n");
+		sc.nextLine();			
+  }
+
+}
